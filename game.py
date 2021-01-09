99b1c1656042512b46014a56973f86882d57b381
evanse10 <evanse10@mcmaster.ca>
2021-01-08 22:02:35 -0700

    added some facts

--- game.py
+++ game.py
@@ -1,12 +1,37 @@
 ## game functions:
 ##display
 
+## FACTS
+## The average college student produces 640 pounds of solid waste each year, including rougly 500 disposable cups and 320 pounds of paper
+## By 2050, the ocean will contain more plastic by weight than fish
+## In 2018, americans disposed of 146.2 million tons of trash - 24 percent was food waste (35.28 million tons)
+## The Geat Pacific Garbage Patch contains almost 3.5 million tons of trash
 
 import pygame
 import time
 import random
  
+pygame.init()
 
 
-hello from liz
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
 
+
+def display():
+    while True:
+        dis = pygame.display.set_mode((600, 600))
+        pygame.display.set_caption("Shehacks 2021")
+        
+
+
+
+
+display()
+
+
+##hello from liz
